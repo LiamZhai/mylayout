319d1de372ffc4450302728cd1b0c6b8d27cc4ec
Liam Zhai <liamzhai@gmail.com>
2017-06-30 12:16:53 +0000

    Add carousel

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Zhengzhou Kangyun Information technolog Co.,Ltd.</title>
+　　　　　<link rel="stylesheet" href="/css/app.css">
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<div id="logo"><img src="images/logo.png"></div>
+				<div id="banner">
+					<div id="left" class="bgcolor">
+                                        
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="2000">
+    <!-- 轮播（Carousel）指标 -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>   
+    <!-- 轮播（Carousel）项目 -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="images/carousel1.jpg" alt="First slide">
+            <div class="carousel-caption">大数据</div>
+        </div>
+        <div class="item">
+            <img src="images/carousel2.jpg" alt="Second slide">
+            <div class="carousel-caption">云平台</div>
+        </div>
+        <div class="item">
+            <img src="images/carousel3.jpg" alt="Third slide">
+            <div class="carousel-caption">全民医疗</div>
+        </div>
+    </div>
+    <!-- 轮播（Carousel）导航 -->
+    <a class="carousel-control left" href="#myCarousel" 
+        data-slide="prev">&lsaquo;
+    </a>
+    <a class="carousel-control right" href="#myCarousel" 
+        data-slide="next">&rsaquo;
+    </a>
+</div>
+
+
+
+
+
+                                        </div>
+					<div id="right" class="bgcolor">
+                                        <img src="images/big_data2.png">
+                                        </div>
+				</div>
+			</div>
+			<div class="nav"> </div>
+			<div id="menu">
+　　　　　　　　　　　　　　
+
+        <div class="row">
+        <div class="col-md-2"><a href="https://www.google.com" target="_blank">公司简介</a></div>
+        <div class="col-md-2"><a href="google.com" target="_blank">经营战略</a></div>
+        <div class="col-md-2"><a href="google.com" target="_blank">产品案例</a></div>
+        <div class="col-md-2"><a href="google.com" target="_blank">公司新闻</a></div>
+        <div class="col-md-2"><a href="google.com" target="_blank">企业荣誉</a></div>
+        <div class="col-md-2"><a href="google.com" target="_blank">发展动态</a></div>
+      </div>
+        </div>
+			<div class="nav"> </div>
+                       <div id="sidebar">
+				<div class="bar border">
+                                <div class="bar-title">合理分配医疗资源</div>
+                                <div class="bar-title-pic"> <img src="images/yiliao1.jpg"></div>
+                                </div>
+				<div class="nav"> </div>
+				<div class="bar border">
+                                <div class="bar-title">提供远程医疗服务</div>
+                                <div class="bar-title-pic"> <img src="images/yiliao2.jpg"></div>
+                                </div>
+				<div class="nav"> </div>
+				<div class="bar border">
+                                <div class="bar-title">提升医院管理水平</div>
+                                <div class="bar-title-pic"><img src="images/yiliao3.jpg"></div>
+                                </div>
+			</div>
+			<div id="content">
+				<div class="left_box border">
+                                 <img src="images/lanmu1.jpg">   
+                                </div>
+				<div class="right_box border">
+                                 <img src="images/lanmu2.jpg">
+                                </div>
+				<div class="nav"> </div>
+				<div class="left_box">
+					<div class="left border">
+                                         <h5>Web application:</h5> 
+                                         <p class="navbar-text navbar-left">Offical website</p>
+                                         <p class="navbar-text navbar-left">CMS &nbsp;&nbsp;&nbsp;&nbsp;</p>
+                                         <p class="navbar-text navbar-left">WMS &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                                         <p class="navbar-text navbar-left">API &nbsp;&nbsp;&nbsp;&nbsp;</p>
+                                        </div>
+					<div class="right border">
+                                        <h5>Technology:</h5>
+                                         <p class="navbar-text navbar-left">Bootstrap,Less</p>
+                                         <p class="navbar-text navbar-left">Laravel,Venus</p>
+                                         <p class="navbar-text navbar-left">Ajax,Json</p>
+                                         <p class="navbar-text navbar-left">Java,SSH</p>
+　　　　　　　　　　　　　　　　　　　　</div>
+				</div>
+				<div class="right_box">
+					<div class="left border">
+                                         <h5>Mobile application:</h5>
+                                         <p class="navbar-text navbar-left">iOS applications</p>
+                                         <p class="navbar-text navbar-left">Android applications</p>
+                                         <p class="navbar-text navbar-left">Wechat applications</p>
+                                         <p class="navbar-text navbar-left">Shopping on mobile</p>
+                                        </div>
+					<div class="right border">
+                                         <h5>Technology:</h5>
+                                         <p class="navbar-text navbar-left">Objective-c</p>
+                                         <p class="navbar-text navbar-left">Java for Android</p>
+                                         <p class="navbar-text navbar-left">Load balancing</p>
+                                         <p class="navbar-text navbar-left">Hybrid App</p>
+                                        </div>
+				</div>
+			</div>
+			<div class="nav"> </div>
+			<div id="footer" class="bgcolor">
+  			
+　　　　　　　　　　 	　　　<p>Fax: &nbsp;+(86)0371-2222-6666</p>		   
+　　　　　　　　　　　　　　  <p>Phone: &nbsp;+(86)136-2222-6666</p>
+  		　　　　　　　<p>Copyright&copy; 2017 Kyit. All Rights Reserved.</p>　　　　
+　　　　　　　　　　</div>
+  		        	  	
+		</div>
+         <script src="/js/app.js"></script>
+	</body>
+
+
+</html>
+
